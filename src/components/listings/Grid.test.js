2c05a0323dc2f951d3c19fe628aa7b59e5e8e224
Lawrence LCI <lcheunglci@gmail.com>
2022-02-25 00:48:34 -0800

    Add more tests

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -9,3 +9,31 @@ test('Number of items = 12', () =>
 test('Number of items to be greater than or equal to 12', () => {
     expect(numItems).toBeGreaterThanOrEqual(12);
 })
+
+const dataTest = data[0].title;
+
+test('There is a JS in this title', () => {
+    expect(dataTest).toMatch(/JS/);
+});
+
+test('The title contains React', () => {
+    expect(dataTest).toContain('React');
+});
+
+// Arrays
+const data2 = ['React Native', 'React'];
+
+test('The list of courses contains Reactive native and React', () => {
+    expect(['React Native', 'React', 'MeteorJS']).toEqual(expect.arrayContaining(data2));
+});
+
+// Objects
+test('The first course to have a property title', ()=> {
+    expect(data[0]).toHaveProperty('title');
+});
+
+// Objects
+test('The first course to have a property title and value of 31,266', ()=> {
+    expect(data[0]).toHaveProperty('views', '31,266');
+});
+
