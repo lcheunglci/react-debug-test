3007ab778f9325119349f1c5c938075abe305b5f
Lawrence LCI <lcheunglci@gmail.com>
2022-02-26 00:26:21 -0800

    Add prop-types and fix the grid

--- src/components/listings/Grid.js
+++ src/components/listings/Grid.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Single from './Single';
 
-export default class Grid extends Component {
-  renderList() {
-    return this.props.items.map((item) => (
-      <Single key={item.id} item={item}/>
-    ));
-  }
+const Grid = function ({items}) {
 
-  render() {
-    return(
-      <div>
-        <div className="row">
-          <ul>
-            {this.renderList()}
-          </ul>
-        </div>
+  return(
+    <div>
+      <div className="row">
+        <ul>
+          {items.map((item) => <Single key={item.id} item={item}/> )}
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Grid.propTypes = {
   items: PropTypes.array.isRequired
 };
+
+export default Grid;
