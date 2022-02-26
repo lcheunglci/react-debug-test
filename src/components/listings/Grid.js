1c124c1d0e73a29032540b8c896faa9646591d7a
Lawrence LCI <lcheunglci@gmail.com>
2022-02-26 00:43:56 -0800

    Fix some linter errors

--- src/components/listings/Grid.js
+++ src/components/listings/Grid.js
@@ -2,21 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Single from './Single';
 
-const Grid = function ({items}) {
-
-  return(
+const Grid = function ({ items }) {
+  return (
     <div>
       <div className="row">
         <ul>
-          {items.map((item) => <Single key={item.id} item={item}/> )}
+          { items.map((item) => <Single key={item.id} item={item} />) }
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Grid.propTypes = {
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
 };
 
 export default Grid;
