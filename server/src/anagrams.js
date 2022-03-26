e5eec5b540040187a9afe54ad5facc8a3735910e
Lawrence LCI <lcheunglci@gmail.com>
2022-03-26 15:11:04 -0700

    Add a new test example requirements

---
+++ server/src/anagrams.js
@@ -0,0 +1,13 @@
+// elbow - below -> true
+// elbows - below -> false
+
+// listen - silent -> true
+// listens - silent -> false
+
+// spaces are ignored and (bonus) ignore all non-alphanumeric values
+// conversation - voices rant on -> true
+
+// case insensitive
+// STATE - taste -> true
+
+// HINT: use getLetterCount function
