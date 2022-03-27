77aaa168db0ec9f58eb731ce707a9d2407504cb4
Lawrence LCI <lcheunglci@gmail.com>
2022-03-26 21:46:14 -0700

    Fix the eol so it uses the unix ones

--- server/src/letter-count.test.js
+++ server/src/letter-count.test.js
@@ -1,28 +1,41 @@
-import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { getLetterCount } from './letter-count';
+import { expect } from 'chai';
+import { isAnagram } from './anagrams';
 
-describe('getLetterCount - basic functionality', () => {
-  it('returns an empty object when passed an empty string', () => {
-    const expected = {};
-    const actual = getLetterCount('');
-    expect(actual).to.deep.equal(expected);
+describe('Verify anagram functionality', () => {
+  it('return true if pass in empty string', () => {
+    const expected = true;
+    const actual = isAnagram('', '');
+    expect(actual).equal(expected);
   });
 
-  it('return the correct letter count for a word with only one of each letter', () => {
-    const expected = { c: 1, a: 1, t: 1 };
-    const actual = getLetterCount('cat');
-    expect(actual).to.deep.equal(expected);
+  it('return true if the two strings contains the same characters', () => {
+    const expected = true;
+    const actual = isAnagram('abc', 'cab');
+    expect(actual).equal(expected);
   });
 
-  it('return the correct letter count for words with more than one of certain letters', () => {
-    const expected = {
-      b: 1,
-      e: 2,
-      t: 2,
-      r: 1,
-    };
-    const actual = getLetterCount('better');
-    expect(actual).to.deep.equal(expected);
+  it('return false if the string does not contain the same characters', () => {
+    const expected = false;
+    const actual = isAnagram('abc', 'def');
+    expect(actual).equal(expected);
+  });
+
+  it('return false if the string does not contain the same character count', () => {
+    const expected = false;
+    const actual = isAnagram('abc', 'abca');
+    expect(actual).equal(expected);
+  });
+
+  it('return true if the string contain the same characters case insensitively', () => {
+    const expected = true;
+    const actual = isAnagram('abc', 'ABC');
+    expect(actual).equal(expected);
+  });
+
+  it('return true if the string contains spaces with the same characters', () => {
+    const expected = true;
+    const actual = isAnagram('abc', 'a b c');
+    expect(actual).equal(expected);
   });
 });
