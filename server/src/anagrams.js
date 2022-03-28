2984f6820dec9d26943a2bb9ecd36bd915b01f9f
Lawrence LCI <lcheunglci@gmail.com>
2022-03-27 23:02:32 -0700

    Refactor anagram and tests

--- server/src/anagrams.js
+++ server/src/anagrams.js
@@ -1,27 +1,22 @@
 import { getLetterCount } from './letter-count';
-
-// elbow - below -> true
-// elbows - below -> false
-
-// listen - silent -> true
-// listens - silent -> false
-
-// spaces are ignored and (bonus) ignore all non-alphanumeric values
-// conversation - voices rant on -> true
-
-// case insensitive
-// STATE - taste -> true
+// import { isEqual } from 'lodash'; // for checking for deep equal
 
 // HINT: use getLetterCount function
+
+export const formatString = (s) => s.toLowerCase().replace(/\s+/g, '');
 
 export const isAnagram = (left, right) => {
   if (left === right) return true;
 
-  const leftCount = getLetterCount(left.toLowerCase().replace(/\s+/g, ''));
-  const rightCount = getLetterCount(right.toLowerCase().replace(/\s+/g, ''));
+  const leftCount = getLetterCount(formatString(left));
+  const rightCount = getLetterCount(formatString(right));
+
+  // if using lodash
+  // return isEqual(leftCount, rightCount);
+
   const leftValues = Object.entries(leftCount).sort().toString();
   const rightValues = Object.entries(rightCount).sort().toString();
   return leftValues === rightValues;
 };
 
-export default isAnagram;
+export default { isAnagram, formatString };
--- server/src/anagrams.test.js
+++ server/src/anagrams.test.js
@@ -1,6 +1,15 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { isAnagram } from './anagrams';
+import { isAnagram, formatString } from './anagrams';
+
+describe('formatString - basic functionality', () => {
+  it('removes spaces and converts to lower case', () => {
+    // 'This is a STRING' -> 'thisisastring'
+    const expected = 'thisisastring';
+    const actual = formatString('This is a STRING');
+    expect(actual).to.equal(expected);
+  });
+});
 
 describe('Verify anagram functionality', () => {
   it('return true if pass in empty string', () => {
@@ -9,33 +18,41 @@ describe('Verify anagram functionality', () => {
     expect(actual).equal(expected);
   });
 
-  it('return true if the two strings contains the same characters', () => {
+  // elbow - below -> true
+  // listen - silent -> true
+  it('return true when the two known anagram are passed in', () => {
     const expected = true;
-    const actual = isAnagram('abc', 'cab');
+    const actual = isAnagram('listen', 'silent');
+    expect(actual).equal(expected);
+  });
+
+  // elbows - below -> false
+  // listens - silent -> false
+  it('return false if the string does not contain the same character count', () => {
+    const expected = false;
+    const actual = isAnagram('listens', 'silent');
     expect(actual).equal(expected);
   });
 
   it('return false if the string does not contain the same characters', () => {
     const expected = false;
-    const actual = isAnagram('abc', 'def');
+    const actual = isAnagram('below', 'elbows');
     expect(actual).equal(expected);
   });
 
-  it('return false if the string does not contain the same character count', () => {
-    const expected = false;
-    const actual = isAnagram('abc', 'abca');
-    expect(actual).equal(expected);
-  });
-
+  // case insensitive
+  // STATE - taste -> true
   it('return true if the string contain the same characters case insensitively', () => {
     const expected = true;
-    const actual = isAnagram('abc', 'ABC');
+    const actual = isAnagram('STATE', 'taste');
     expect(actual).equal(expected);
   });
 
+  // spaces are ignored and (bonus) ignore all non-alphanumeric values
+  // conversation - voices rant on -> true
   it('return true if the string contains spaces with the same characters', () => {
     const expected = true;
-    const actual = isAnagram('abc', 'a b c');
+    const actual = isAnagram('conversation', 'voices rant on');
     expect(actual).equal(expected);
   });
 });
