b79f243ac5b265b02cbf5e6719ce396af9b8dc90
Lawrence LCI <lcheunglci@gmail.com>
2022-03-28 22:41:21 -0700

    Refactor setup and tear down of the db to the test helper

--- server/src/db.test.js
+++ server/src/db.test.js
@@ -1,23 +1,10 @@
-import { MongoClient } from 'mongodb';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { getUserByUsername } from './db';
-
-const DB_NAME = process.env.NODE_ENV === 'test' ? 'TEST_DB' : 'PROD_DB';
+import { getDatabaseData, resetDatabase, setDatabaseData } from './test-helper';
 
 describe('getUserByUsername', () => {
   it('get the correct user from the database given a username', async () => {
-    const client = await MongoClient.connect(
-      `mongodb://localhost:27017/${DB_NAME}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    );
-
-    const db = client.db(DB_NAME);
-    // tests
-
     const fakeData = [
       {
         id: '123',
@@ -31,12 +18,11 @@ describe('getUserByUsername', () => {
       },
     ];
 
-    await db.collection('users').insertMany(fakeData);
+    await setDatabaseData('users', fakeData);
 
     const actual = await getUserByUsername('abc');
-    const finalDBState = await db.collection('users').find().toArray();
-    await db.dropDatabase();
-    client.close();
+    const finalDBState = await getDatabaseData('users');
+    await resetDatabase();
 
     const expected = {
       id: '123',
